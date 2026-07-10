c7d7098afc69a9a68924fd4ff8a4d2811511696b
yogeshitinfra <vanarseyogesh82@gmail.com>
2026-07-10 13:26:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+emailjs.init("TlU-DcrSvloKgM97M");
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    emailjs.sendForm(
+        "service_hzmm3sg",
+        "template_tis4anc",
+        this
+    ).then(function() {
+        alert("Message Sent Successfully!");
+        form.reset();
+    }, function(error) {
+        alert("Failed to send message.");
+        console.log(error);
+    });
+});
