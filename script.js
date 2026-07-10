ae823a11b71f074aa96141e38548521f62fa613c
yogeshitinfra <vanarseyogesh82@gmail.com>
2026-07-10 20:17:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,30 @@ emailjs.init("TlU-DcrSvloKgM97M");
 
 const form = document.getElementById("contact-form");
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
+if (form) {
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
 
-    emailjs.sendForm(
-        "service_hzmm3sg",
-        "template_tis4anc",
-        this
-    ).then(function() {
-        alert("Message Sent Successfully!");
-        form.reset();
-    }, function(error) {
-        alert("Failed to send message.");
-        console.log(error);
+        emailjs.sendForm(
+            "service_hzmm3sg",
+            "template_tis4anc",
+            this
+        ).then(function() {
+            alert("Message Sent Successfully!");
+            form.reset();
+        }, function(error) {
+            alert("Failed to send message.");
+            console.log(error);
+        });
     });
-});
+}
+
+
 const menuToggle = document.getElementById("menu-toggle");
 const nav = document.querySelector("nav");
 
-menuToggle.addEventListener("click", function () {
-    nav.classList.toggle("active");
-});
+if (menuToggle && nav) {
+    menuToggle.addEventListener("click", function () {
+        nav.classList.toggle("active");
+    });
+}
