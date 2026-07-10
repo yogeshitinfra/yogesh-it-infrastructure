a58e6a76e918bffd03ad267d9d331a6f96180c89
yogeshitinfra <vanarseyogesh82@gmail.com>
2026-07-10 20:21:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,11 @@ if (menuToggle && nav) {
         nav.classList.toggle("active");
     });
 }
+const menuToggle = document.getElementById("menu-toggle");
+const nav = document.querySelector("nav");
+
+if(menuToggle && nav){
+    menuToggle.addEventListener("click", function(){
+        nav.classList.toggle("active");
+    });
+}
